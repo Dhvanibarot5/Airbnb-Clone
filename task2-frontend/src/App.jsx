7874b2bab85861ea03154f999611a6d98010973f
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-25 01:25:38 +0530

    hero section navbar started

--- task2-frontend/src/App.jsx
+++ task2-frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import HeroSecNavbar from "./components/HeroSecNavbar";
 
 function App() {
   return (
     <>
       <Header />
-      
+      <HeroSecNavbar />
     </>
   );
 }
